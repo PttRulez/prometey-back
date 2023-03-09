a66d701d9662eae8ba74516d4e177e05c867cdd4
petya <sashafem@mail.ru>
2023-03-09 16:30:55 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:*', 'http://localhost:*'],
+    'allowed_origins' => ['https://prometey.initup.ru'],
 
     'allowed_origins_patterns' => [],
 
